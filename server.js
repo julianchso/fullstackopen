7224fa2da674af3b9e1da97247d54e6c4046051d
Julian So <ch.julian.so@gmail.com>
2021-06-19 17:59:57 -0400

    learn how to return a request in api

--- server.js
+++ server.js
@@ -5,28 +5,33 @@ const PORT = process.env.PORT || 3001;
 
 app.use(cors());
 
-let persons = {
-  "Arto Hellas": {
+let persons = [
+  {
     id: 1,
     name: "Arto Hellas",
     number: "040-123456",
   },
-  "Ada Lovelace": {
+  {
     id: 2,
     name: "Ada Lovelace",
     number: "39-44-5323523",
   },
-  "Dan Abramov": {
+  {
     id: 3,
     name: "Dan Abramov",
     number: "12-43-234345",
   },
-  "Mary Poppendieck": {
+  {
     id: 4,
     name: "Mary Poppendieck",
     number: "39-23-6423122",
   },
-};
+  {
+    id: 0,
+    name: "unknown",
+    number: "unknown",
+  },
+]
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -36,9 +41,36 @@ app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
-app.get("/api/persons/:personName", (req, res) => {
-  const 
-})
+app.get("/api/persons/:id", (req, res) => {
+  // const personName = req.params.person.toLowerCase();
+
+  // console.log(personName);
+  // console.log(persons);
+  // console.log(persons[personName]);
+
+  // if (persons[personName]) {
+  //   res.json(persons[personName]);
+  // } else {
+  //   res.json(persons["unknown"]);
+  // }
+
+  // const id = Number(req.params.id);
+  const id = Number(req.params.id);
+  const person = persons.find(person => person.id === id)
+  const unknown = persons.id == "unknown"
+  console.log(unknown)
+
+  console.log(persons[id]);
+  console.log(id)
+
+  res.json(person)
+  
+  // if (persons[id]) {
+  //   // res.json(persons[id]);
+  // } else {
+  //   // res.json(person[unknown]);
+  // }
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
